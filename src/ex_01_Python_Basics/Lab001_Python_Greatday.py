e6d2ee2be6ca37185f8e14c063d69d9ca6ca1b94
DAKSHAYANI Y <dakshayani497@gmail.com>
2025-11-12 09:24:05 +0530

    basics

--- src/ex_01_Python_Basics/Lab001_Python_Greatday.py
+++ src/ex_01_Python_Basics/Lab001_Python_Greatday.py
@@ -1,1 +1,7 @@
 print("Have a great day all")
+print("Poornima, Navya, Meena, Priya")
+
+print("Navya","Poori","Meena","Priya","Daksha", sep="&")
+print("Appa","Amma","Daksha")
+print("appa","amma","daksha","bhavani","renuka","kalakka")
+print("lion","tiger","cheetah", sep="$")
